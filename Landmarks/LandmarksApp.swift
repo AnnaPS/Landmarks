34929ce2ec9473cdd53a1cece4396f5d35a9892f
Ana Polo <anna-91_@hotmail.com>
2025-04-27 16:46:59 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ana Polo  on 27/4/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
