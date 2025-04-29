76a54ad27ce1706683a26902e71e200c08ffd09b
Ana Polo <anna-91_@hotmail.com>
2025-04-29 17:25:08 +0200

    feat: add watchOS target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Ana Polo  on 29/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
